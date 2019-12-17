1dc83b7ea0c5ac33c63524a32dd04e5ea5ed7e9d
Lipien <adam@pawtel.pl>
2019-12-17 08:12:05 +0100

    Statements refactor.

--- src/statements/Statements.java
+++ src/statements/Statements.java
@@ -1,9 +1,31 @@
 package statements;
 
 import java.security.SecureRandom;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Statements {
+    public enum Color {
+        BLACK, ORANGE
+    }
+
     public static void main(String[] args) {
+
+        Map<Color, String> mapa = new EnumMap<Color, String>(Color.class);
+        mapa.put(Color.BLACK, "Czerwony");
+
+        Random random = new Random();
+        int rand = random.nextInt() < 10 ? 5 : 20;
+        System.out.println(rand);
+
+        if (rand > 20)
+            System.out.println("TUTU");
+        else if (rand < 5)
+            System.out.println("PAPA");
+        else
+            System.out.println("RUTU");
+
         SecureRandom secureRandom = new SecureRandom();
         int r = secureRandom.nextInt(20);
         if (r > 4) {
@@ -15,5 +37,14 @@ public class Statements {
         } else {
             System.out.println("I don't know...");
         }
+
+        int hourOfDay = 10;
+        if (hourOfDay < 11) {
+            System.out.println("Good morning");
+            System.out.println("What a nice day");
+        }
+
+        int y = 5;
+        System.out.println((y > 5) ? 21 : "zebra");
     }
 }
